2145ed4fc1191f094e09f359f1ebfa1af86336f3
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2013-12-07 21:41:57 -0300

    First commit - scaffolded basic rails app

--- README.md
+++
@@ -1,4 +0,0 @@
-sicablog
-========
-
-Sicarul's Blog
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sicablog::Application.config.session_store :cookie_store, key: '_sicablog_session'
